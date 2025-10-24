e44b7182d34119e44d44e67bc368f083eaa84a95
CHAOS 131 <131.chaos@gmail.com>
2025-10-24 16:32:52 -0400

    Floor Intake Pose Adjust

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -114,7 +114,7 @@ public final class Constants {
 
     public static class ArmPoses {
       public static final DashboardUnit<AngleUnit, Angle> StartingPose = new DashboardUnit<>("Poses/Starting", Degrees.of(90)); //TODO Tune all of these
-      public static final DashboardUnit<AngleUnit, Angle> FloorIntakePose = new DashboardUnit<>("Poses/FloorIntake", Degrees.of(214));
+      public static final DashboardUnit<AngleUnit, Angle> FloorIntakePose = new DashboardUnit<>("Poses/FloorIntake", Degrees.of(213));
       public static final DashboardUnit<AngleUnit, Angle> HPIntakePose = new DashboardUnit<>("Poses/HPIntake", Degrees.of(115));
       public static final DashboardUnit<AngleUnit, Angle> ScoreLowPose = new DashboardUnit<>("Poses/ScoreLow", Degrees.of(12));
       public static final DashboardUnit<AngleUnit, Angle> ScoreHighPose = new DashboardUnit<>("Poses/ScoreHigh", Degrees.of(22));
